22599e58faa300c2c60799e72ae572faacb9105d
Yu Tong Liu <yutongliuytl@gmail.com>
2023-09-07 20:40:18 -0400

    Initialize temporary home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="textContainer">
+        <p className="subtitle">a design space based in montreal</p>
+        <h1 className="title">whiteee space</h1>
+      </div>
+      <a
+        href={"mailto:info@whiteee.space&subject=Saying hi!"}
+        className="email"
+      >
+        info@whiteee.space
+      </a>
     </div>
   );
 }
