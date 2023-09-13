3e09f506ec88ac6d718f4e9669eb1c59721045b7
Yu Tong Liu <yutongliuytl@gmail.com>
2023-09-12 20:36:24 -0400

    Testing animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,64 @@
-import { motion } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
 
 import "./App.css";
+import { useCallback, useEffect, useState } from "react";
 
 function App() {
+  const [scope, animate] = useAnimate();
+  const [startRepeatAnimation, setStartRepeatAnimation] = useState(true);
+
+  const handleHoverStart = useCallback(() => {
+    animate(
+      scope.current,
+      { scale: [1], width: [36, 180], borderRadius: [50, 20] },
+      { ease: "easeInOut", duration: 0.5 }
+    );
+    animate(
+      "div",
+      { opacity: [0, 0, 0, 1] },
+      { ease: "easeInOut", duration: 0.5 }
+    );
+  }, [animate, scope]);
+
+  const handleHoverEnd = useCallback(async () => {
+    animate(
+      scope.current,
+      { scale: [1], width: [180, 36], borderRadius: [20, 50] },
+      { ease: "easeInOut", duration: 0.5 }
+    );
+    animate(
+      "div",
+      { opacity: [1, 0, 0, 0] },
+      { ease: "easeInOut", duration: 0.5 }
+    ).then(() => {
+      setStartRepeatAnimation(true);
+    });
+  }, [animate, scope]);
+
+  useEffect(() => {
+    if (startRepeatAnimation) {
+      animate("div", { opacity: [0] });
+      animate(
+        scope.current,
+        { scale: [1, 1, 1, 1.2, 1, 1, 1], width: [36], borderRadius: [50] },
+        { ease: "easeInOut", repeat: Infinity, duration: 2 }
+      );
+      setStartRepeatAnimation(false);
+    }
+  }, [animate, scope, startRepeatAnimation]);
+
   return (
     <div className="container">
+      <div
+        ref={scope}
+        onMouseEnter={handleHoverStart}
+        onMouseLeave={handleHoverEnd}
+        className="dotLogo"
+      >
+        <div className="logoText" style={{ opacity: 0, marginRight: 15 }}>
+          whiteee space
+        </div>
+      </div>
       <div className="textContainer">
         <motion.p
           initial={{ opacity: 0 }}
